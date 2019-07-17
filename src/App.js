1ef918a5edfc2384cfedf265dabe8e290d695f61
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-07-17 18:30:00 -0400

    reached mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import Component from "./Component";
+import axios from "axios"
 
 function App() {
+
+  const [photos, setPhotos] = useState([]);
+
+  useEffect( () => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then( (res) => {
+        setPhotos(res.data)
+      })
+      .catch( err => console.log("Oopsies!: ", err))
+  }, [] )
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Component url={photos.url} title={photos.title} explanation={photos.explanation} />
     </div>
   );
 }
